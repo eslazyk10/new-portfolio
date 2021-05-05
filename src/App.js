82708b8fc9d5d712d1146dc7ab5640495a1fbee0
eslazyk10 <eslazy32@gmail.com>
2021-05-05 14:54:12 -0500

    created app
    created react app, created components for navbar and intro, imported personalized logo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Intro from "./Intro";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
