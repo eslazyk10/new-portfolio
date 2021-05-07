9987ea5c13bac2a49b4cb3bba674a1c4cc63858e
eslazyk10 <eslazy32@gmail.com>
2021-05-06 22:12:46 -0500

    modified components to display about me section after intro, updated css

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,9 +1,52 @@
 import React from "react";
+import Me from "./images/resume-img.jpg";
+import bootstrap from "./images/bootstrap-logo.png";
+import css from "./images/CSS-logo.png";
+import react from "./images/React-logo.png";
+import js from "./images/Javascript-logo.png";
+import responsive from "./images/responsive-logo.png";
+import html from "./images/htmlLogo.png";
+
+import "./AboutMe.css";
 
 export default function AboutMe(){
     return(
         <div className="AboutMe">
-            
+            <h2 className="about">About Me</h2>
+            <div className="row">
+                <div className="col-sm-5">
+            <img className="portrait" src={Me} alt="Me"/>
+            </div>
+            <div className="col-sm-7 skills">
+                <h3>Technologies I use</h3>
+                <div className="tech-block">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-2">
+                            <img src={html} alt="" />
+                        </div>
+                        <div className="col-md-2">
+                            <img src={css} alt="" />
+                        </div>
+                        <div className="col-md-2">
+                            <img src={js} alt="" />
+                        </div>
+                        </div>
+                        <div className="row">
+                        <div className="col-md-2">
+                            <img src={react} alt="" />
+                        </div>
+                        <div className="col-md-2">
+                            <img src={bootstrap} alt="" />
+                        </div>
+                        <div className="col-md-2">
+                            <img src={responsive} alt="" />
+                        </div>
+                    </div>
+                </div>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Intro.js
+++ src/Intro.js
@@ -1,10 +1,5 @@
 import React from "react";
-import bootstrap from "./images/bootstrap-logo.png";
-import css from "./images/CSS-logo.png";
-import react from "./images/React-logo.png";
-import js from "./images/Javascript-logo.png";
-import responsive from "./images/responsive-logo.png";
-import html from "./images/htmlLogo.png";
+
 
 import "./Intro.css";
 
@@ -15,33 +10,6 @@ export default function Intro(){
             <h1 className="name">Emily Slazyk</h1>
             <h3 className="position"><i class="fas fa-angle-left"></i>Front End Developer/<i class="fas fa-angle-right"></i></h3>
             <h5 className="location">Based in Nashville, TN</h5>
-            </section>
-            <section className="skills">
-                <h3>Technologies I use</h3>
-                <div className="tech-block">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-2">
-                            <img src={html} alt="" />
-                        </div>
-                        <div className="col-md-2">
-                            <img src={css} alt="" />
-                        </div>
-                        <div className="col-md-2">
-                            <img src={js} alt="" />
-                        </div>
-                        <div className="col-md-2">
-                            <img src={react} alt="" />
-                        </div>
-                        <div className="col-md-2">
-                            <img src={bootstrap} alt="" />
-                        </div>
-                        <div className="col-md-2">
-                            <img src={responsive} alt="" />
-                        </div>
-                    </div>
-                </div>
-                </div>
             </section>
         </div>
     );
