eb6b7f99a35244ce62f73bc472dd152ebfa83b41
eslazyk10 <eslazy32@gmail.com>
2021-05-13 13:01:08 -0500

    added content and links to contact page

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,21 @@ import React from "react";
 export default function Contact(){
     return(
         <div className="Contact">
-            hello
+            <h1>🙋‍♀️Let's get in touch</h1>
+            <h3>You can reach me through any of the links below!</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <a href="https://www.linkedin.com/in/emily-slazyk-9b4b16185/" className="contactLinks"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                    <div className="col">
+                        <a href="https://github.com/eslazyk10" className="contactLinks"><i class="fab fa-github"></i></a>
+                    </div>
+                    <div className="col">
+                        <a href="mailto:eslazy32@gmail.com" className="contactLinks"><i class="fas fa-envelope"></i></a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ export default function Header(){
         <div className="Header">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src={logo} alt="Logo" width="55"/></a>
+    <Link to="/home"><img src={logo} alt="Logo" width="55"/></Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
